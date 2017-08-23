84436d140c4a6279c9208d45bde8ff4377da1315
Jenny H <jennyhong.jy@gmail.com>
2017-08-23 18:12:23 +0900

    Login - halfway through

--- coderaising-mongodb/react-client/dist/bundle.js
+++ coderaising-mongodb/react-client/dist/bundle.js
@@ -28845,35 +28845,122 @@
   \**********************************************/
 /***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
 	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(/*! react */ 1);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
+	var _reactGoogleLoginComponent = __webpack_require__(/*! react-google-login-component */ 263);
+	
+	var _axios = __webpack_require__(/*! axios */ 231);
+	
+	var _axios2 = _interopRequireDefault(_axios);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	var Login = function Login() {
-	  return _react2.default.createElement(
-	    "div",
-	    null,
-	    "Username: ",
-	    _react2.default.createElement("input", { type: "text", id: "username-input" }),
-	    _react2.default.createElement("br", null),
-	    "Password: ",
-	    _react2.default.createElement("input", { type: "text", id: "password-input" }),
-	    _react2.default.createElement("br", null),
-	    _react2.default.createElement(
-	      "button",
-	      { type: "button" },
-	      "Log In"
-	    )
-	  );
-	};
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Login = function (_React$Component) {
+	  _inherits(Login, _React$Component);
+	
+	  function Login(props, context) {
+	    _classCallCheck(this, Login);
+	
+	    return _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).call(this, props, context));
+	  }
+	
+	  _createClass(Login, [{
+	    key: 'responseGoogle',
+	    value: function responseGoogle(googleUser) {
+	      var id_token = googleUser.getAuthResponse().id_token;
+	      console.log({ accessToken: id_token });
+	      //anything else you want to do(save to localStorage)...
+	
+	      // axios.post('http://localhost:8000/newpost', newpost)
+	      //   .then(response => {
+	      //     console.log('Post has been submitted!');
+	      //     alert('Your post has been submitted!');
+	      //   })
+	      //   .then(() => {
+	      //     this.props.history.push('/posts');
+	      //   })
+	      //   .catch(response => console.log('Failed to post'));
+	      //
+	    }
+	  }, {
+	    key: 'onSignIn',
+	    value: function onSignIn(googleUser) {
+	      var profile = googleUser.getBasicProfile();
+	      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	      console.log('Name: ' + profile.getName());
+	      console.log('Image URL: ' + profile.getImageUrl());
+	      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	    }
+	  }, {
+	    key: 'signOut',
+	    value: function signOut() {
+	      var auth2 = gapi.auth2.getAuthInstance();
+	      auth2.signOut().then(function () {
+	        console.log('User signed out.');
+	      });
+	    }
+	  }, {
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      gapi.signin2.render('g-signin2', {
+	        'scope': 'profile email',
+	        'width': 200,
+	        'height': 50,
+	        'longtitle': true,
+	        'theme': 'dark',
+	        'onsuccess': this.onSignIn
+	      });
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        'Username: ',
+	        _react2.default.createElement('input', { type: 'text', id: 'username-input' }),
+	        _react2.default.createElement('br', null),
+	        'Password: ',
+	        _react2.default.createElement('input', { type: 'text', id: 'password-input' }),
+	        _react2.default.createElement('br', null),
+	        _react2.default.createElement(
+	          'button',
+	          { type: 'button' },
+	          'Log In'
+	        ),
+	        _react2.default.createElement('p', null),
+	        _react2.default.createElement('div', { id: 'g-signin2', 'data-onsuccess': this.onSignIn }),
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          _react2.default.createElement(
+	            'a',
+	            { href: '#', onClick: this.signOut },
+	            'Google Sign out'
+	          )
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return Login;
+	}(_react2.default.Component);
 	
 	exports.default = Login;
 
@@ -29154,6 +29241,200 @@
 	}(_react2.default.Component);
 	
 	exports.default = EachPost;
+
+/***/ }),
+/* 263 */
+/*!*****************************************************************************!*\
+  !*** ./~/react-google-login-component/dist/react-google-login-component.js ***!
+  \*****************************************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+	(function webpackUniversalModuleDefinition(root, factory) {
+		if(true)
+			module.exports = factory(__webpack_require__(/*! react */ 1));
+		else if(typeof define === 'function' && define.amd)
+			define(["react"], factory);
+		else if(typeof exports === 'object')
+			exports["react-google-login-component"] = factory(require("react"));
+		else
+			root["react-google-login-component"] = factory(root["React"]);
+	})(this, function(__WEBPACK_EXTERNAL_MODULE_2__) {
+	return /******/ (function(modules) { // webpackBootstrap
+	/******/ 	// The module cache
+	/******/ 	var installedModules = {};
+	
+	/******/ 	// The require function
+	/******/ 	function __webpack_require__(moduleId) {
+	
+	/******/ 		// Check if module is in cache
+	/******/ 		if(installedModules[moduleId])
+	/******/ 			return installedModules[moduleId].exports;
+	
+	/******/ 		// Create a new module (and put it into the cache)
+	/******/ 		var module = installedModules[moduleId] = {
+	/******/ 			exports: {},
+	/******/ 			id: moduleId,
+	/******/ 			loaded: false
+	/******/ 		};
+	
+	/******/ 		// Execute the module function
+	/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+	
+	/******/ 		// Flag the module as loaded
+	/******/ 		module.loaded = true;
+	
+	/******/ 		// Return the exports of the module
+	/******/ 		return module.exports;
+	/******/ 	}
+	
+	
+	/******/ 	// expose the modules object (__webpack_modules__)
+	/******/ 	__webpack_require__.m = modules;
+	
+	/******/ 	// expose the module cache
+	/******/ 	__webpack_require__.c = installedModules;
+	
+	/******/ 	// __webpack_public_path__
+	/******/ 	__webpack_require__.p = "";
+	
+	/******/ 	// Load entry module and return exports
+	/******/ 	return __webpack_require__(0);
+	/******/ })
+	/************************************************************************/
+	/******/ ([
+	/* 0 */
+	/***/ function(module, exports, __webpack_require__) {
+	
+		'use strict';
+	
+		Object.defineProperty(exports, "__esModule", {
+		  value: true
+		});
+		exports.GoogleLogin = undefined;
+	
+		var _GoogleLogin = __webpack_require__(1);
+	
+		var _GoogleLogin2 = _interopRequireDefault(_GoogleLogin);
+	
+		function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+		exports.GoogleLogin = _GoogleLogin2.default;
+	
+	/***/ },
+	/* 1 */
+	/***/ function(module, exports, __webpack_require__) {
+	
+		'use strict';
+	
+		Object.defineProperty(exports, "__esModule", {
+		  value: true
+		});
+	
+		var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+		var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+		var _react = __webpack_require__(2);
+	
+		var _react2 = _interopRequireDefault(_react);
+	
+		function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+		function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+	
+		function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+		function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+		function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+		var GoogleLogin = function (_React$Component) {
+		  _inherits(GoogleLogin, _React$Component);
+	
+		  function GoogleLogin(props) {
+		    _classCallCheck(this, GoogleLogin);
+	
+		    return _possibleConstructorReturn(this, (GoogleLogin.__proto__ || Object.getPrototypeOf(GoogleLogin)).call(this, props));
+		  }
+	
+		  _createClass(GoogleLogin, [{
+		    key: 'componentDidMount',
+		    value: function componentDidMount() {
+		      (function (d, s, id) {
+		        var js = void 0,
+		            gs = d.getElementsByTagName(s)[0];
+		        if (d.getElementById(id)) {
+		          return;
+		        }
+		        js = d.createElement(s);js.id = id;
+		        js.src = 'https://apis.google.com/js/platform.js';
+		        gs.parentNode.insertBefore(js, gs);
+		      })(document, 'script', 'google-platform');
+		    }
+		  }, {
+		    key: 'checkLoginState',
+		    value: function checkLoginState(response) {
+		      if (auth2.isSignedIn.get()) {
+		        var profile = auth2.currentUser.get().getBasicProfile();
+		      } else {
+		        if (this.props.responseHandler) {
+		          this.props.responseHandler({ status: response.status });
+		        }
+		      }
+		    }
+		  }, {
+		    key: 'clickHandler',
+		    value: function clickHandler() {
+		      var socialId = this.props.socialId,
+		          responseHandler = this.props.responseHandler,
+		          scope = this.props.scope;
+	
+		      gapi.load('auth2', function () {
+		        var auth2 = gapi.auth2.init({
+		          client_id: socialId,
+		          fetch_basic_profile: false,
+		          scope: scope
+		        });
+		        auth2.signIn().then(function (googleUser) {
+		          responseHandler(googleUser);
+		        });
+		      });
+		    }
+		  }, {
+		    key: 'render',
+		    value: function render() {
+		      var _props = this.props,
+		          socialId = _props.socialId,
+		          scope = _props.scope,
+		          responseHandler = _props.responseHandler,
+		          children = _props.children,
+		          buttonText = _props.buttonText,
+		          props = _objectWithoutProperties(_props, ['socialId', 'scope', 'responseHandler', 'children', 'buttonText']);
+	
+		      return _react2.default.createElement(
+		        'button',
+		        _extends({}, props, { onClick: this.clickHandler.bind(this) }),
+		        children,
+		        buttonText
+		      );
+		    }
+		  }]);
+	
+		  return GoogleLogin;
+		}(_react2.default.Component);
+	
+		exports.default = GoogleLogin;
+	
+	/***/ },
+	/* 2 */
+	/***/ function(module, exports) {
+	
+		module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
+	
+	/***/ }
+	/******/ ])
+	});
+	;
 
 /***/ })
 /******/ ]);
--- coderaising-mongodb/react-client/src/components/Login.js
+++ coderaising-mongodb/react-client/src/components/Login.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login-component';
+import axios from 'axios';
 
-const Login = () => (
-  <div>
-    Username: <input type="text" id="username-input" /><br />
-    Password: <input type="text" id="password-input" /><br />
-    <button type="button">Log In</button>
-  </div>
-);
+class Login extends React.Component{
+  constructor (props, context) {
+    super(props, context);
+  }
+
+  responseGoogle (googleUser) {
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log({accessToken: id_token});
+    //anything else you want to do(save to localStorage)...
+
+    // axios.post('http://localhost:8000/newpost', newpost)
+    //   .then(response => {
+    //     console.log('Post has been submitted!');
+    //     alert('Your post has been submitted!');
+    //   })
+    //   .then(() => {
+    //     this.props.history.push('/posts');
+    //   })
+    //   .catch(response => console.log('Failed to post'));
+    //
+
+  }
+
+  onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  signOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      console.log('User signed out.');
+    });
+  }
+
+  componentDidMount() {
+    gapi.signin2.render('g-signin2', {
+      'scope': 'profile email',
+      'width': 200,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': this.onSignIn
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        Username: <input type="text" id="username-input" /><br />
+        Password: <input type="text" id="password-input" /><br />
+        <button type="button">Log In</button><p/>
+
+        <div id="g-signin2" data-onsuccess={this.onSignIn} />
+
+          <div>
+            <a href="#" onClick={this.signOut}>Google Sign out</a>
+          </div>
+      </div>
+    );
+  }
+
+}
 
 export default Login;
