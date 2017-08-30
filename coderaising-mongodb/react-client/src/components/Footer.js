5af2a739ef4983675501597686fc4a04faed633c
Jenny H <jennyhong.jy@gmail.com>
2017-08-30 18:10:30 +0900

    Finished header

--- coderaising-mongodb/react-client/src/components/Footer.js
+++ coderaising-mongodb/react-client/src/components/Footer.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 
 const Footer = () => {
   return (
     <div id="footer">
-
     ©2017 CODERAISING. ALL RIGHTS RESERVED
     </div>
   );
-
 };
 
 export default Footer;
--- coderaising-mongodb/react-client/src/components/Header.js
+++ coderaising-mongodb/react-client/src/components/Header.js
@@ -5,13 +5,17 @@ import { NavLink } from 'react-router-dom';
 const Header = ({loginStatus, logoutClick}) => {
   return (
     <div id="header">
-      <div className="logo"><NavLink to="/">CodeRaising</NavLink></div>
+      <div className="logo">
+        <NavLink to="/">
+        <span style={{color: '#4e4e4f'}}>Code</span><span style={{color: '#e25e28'}}>Raising</span>
+        </NavLink>
+      </div>
        <div className="menu">
         <NavLink to="/about">About</NavLink>
         <NavLink to="/posts">Posts</NavLink>
         <NavLink to="/charities">Charities</NavLink>
         {!loginStatus
-        ? <span className="login"><NavLink to="/login">Log In with Google</NavLink></span>
+        ? <span className="login"><NavLink to="/login">Login with Google</NavLink></span>
         : <span className="profile-logout"><NavLink to="/profile">Profile</NavLink>
         <a href='#' onClick={() => logoutClick()}>Log Out</a></span>}
       </div>
